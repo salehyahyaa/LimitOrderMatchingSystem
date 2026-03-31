2c168adb507afd258dbb8e5f71ecaea371c50463
Saleh Yahya <salehyahya10.20@gmail.com>
2026-03-31 12:37:04 -0500

    header file 1/3

---
+++ OrderbookLevelinfos.h
@@ -0,0 +1,19 @@
+#pragma once //header file guard
+#include "LevelInfo.h"
+
+class OrderbookLevelInfos {
+  
+  public:
+    OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
+      : bids_ { bids },                                       //intinlizing fields within constrctor 
+        asks_ { asks }
+    { }
+
+    const LevelInfos& GetBids() const { return bids_;}       //public API
+    const LevelInfos& GetAsks() const { return asks_;}      //Public API
+
+
+  private: 
+    LevelInfos bids_;                                        //Private API
+    LevelInfos asks_;                                        //Private API //now we have the interal state of what represents an orderbook such as each row has a ask and bid price
+  };
